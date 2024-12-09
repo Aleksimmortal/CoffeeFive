85089e1f5a9a5c9818e202353203caa08f07556e
Антон <aleksimmortal@mail.ru>
2024-12-09 22:13:40 +0300

    Добавьте файлы проекта.

---
+++ Coffee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeTypes;
+
+public class FiveCofe
+{
+    public bool Sugar { get; set; }
+    public bool Water { get; set; }
+    public string Milk { get; set; }
+    public bool Ice { get; set; }
+
+    public FiveCofe()
+    {
+        Console.WriteLine("Пожалуйста Ваш Чёрный, без всего!!!");
+    }
+
+    public FiveCofe(bool water)
+    {
+        Water = water;
+        Console.WriteLine("Пожалуйста Ваш Американо!");
+    }
+
+    public FiveCofe(bool sugar, string milk)
+    {
+        Sugar = sugar;
+        Milk = milk;
+        Console.WriteLine("Пожалуйста Ваш Купучино!");
+    }
+
+    public FiveCofe(bool sugar, bool water, string milk)
+    {
+        Sugar = sugar;
+        Water = water;
+        Milk = milk;
+        Console.WriteLine("Пожалуйста Ваш Раф!");
+    }
+
+    public FiveCofe(bool sugar, bool water, string milk, bool ice)
+    {
+        Sugar = sugar;
+        Water = water;
+        Milk = milk;
+        Ice = ice;
+        Console.WriteLine("Пожалуйста Ваш Фраппе!");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿namespace CoffeeTypes
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Добро пожаловать в кофейню, у нас есть пять видов кофе, сделайте свой выбор нажав цифры от 1 до 5: \n" +
+                                  "1 - Чёрный\n" +
+                                  "2 - Американо\n" +
+                                  "3 - Капучино\n" +
+                                  "4 - Раф\n" +
+                                  "5 - Фраппе");
+
+                int cofe = Convert.ToInt32(Console.ReadLine());
+                switch (cofe)
+                {
+                    case 1:
+                        var cofe1 = new FiveCofe();
+                        break;
+                    case 2:
+                        var cofe2 = new FiveCofe(true);
+                        break;
+                    case 3:
+                        var cofe3 = new FiveCofe(true, "moloko");
+                        break;
+                    case 4:
+                        var cofe4 = new FiveCofe(true, true, "moloko");
+                        break;
+                    case 5:
+                        var cofe5 = new FiveCofe(true, true, "moloko", true);
+                        break;
+                    default:
+                        Console.WriteLine("Вы ввели неверное значение");
+                        break;
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+};
